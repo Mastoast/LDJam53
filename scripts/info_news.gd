76ce300dc3687179084cacc22697b065c8000d46
Mastoast <dgekec@gmail.com>
2023-04-30 17:23:27 +0200

    build dynamic messages

--- scripts/info_news.gd
+++ scripts/info_news.gd
@@ -2,15 +2,14 @@ extends VBoxContainer
 var origin
 var distance = 500
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	$start_timer.start(1)
 	origin = self.position
 	self.position.x = self.position.x+500
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	if !$end_timer.is_stopped():
 		self.position.x = origin.x + 500 * (1 - $end_timer.time_left)
@@ -22,11 +21,8 @@ func _on_texture_button_pressed():
 	$end_timer.start(1)
 	origin = self.position
 	$start_timer.stop()
-	
-	
-	
 
 
 func _on_end_timer_timeout():
 	self.queue_free()
-	
+
