7c338f828a9480173456737ff2cefa5356b0ae62
Mastoast <dgekec@gmail.com>
2023-05-02 01:54:11 +0200

    add dynamic crt params

---
+++ scripts/crt_filter.gd
@@ -0,0 +1,21 @@
+extends ColorRect
+
+var shader
+
+
+func _ready():
+	shader = self.material
+
+func _process(delta):
+	pass
+
+func _input(event):
+	if event.is_action_pressed("ui_right", true):
+		shader.set_shader_parameter("scanlines_width", shader.get_shader_parameter("scanlines_width") + 0.1)
+	if event.is_action_pressed("ui_left", true):
+		shader.set_shader_parameter("scanlines_width", shader.get_shader_parameter("scanlines_width") - 0.1)
+	if event.is_action_pressed("ui_up", true):
+		shader.set_shader_parameter("scanlines_opacity", shader.get_shader_parameter("scanlines_opacity") + 0.05)
+	if event.is_action_pressed("ui_down", true):
+		shader.set_shader_parameter("scanlines_opacity", shader.get_shader_parameter("scanlines_opacity") - 0.05)
+
